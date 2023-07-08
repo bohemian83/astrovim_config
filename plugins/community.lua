399e168b0ec1b41015a48160046bef2c8c61c2b5
Panagiotis Grigorakis <pgrigorakis@Phantom.local>
2023-07-08 11:56:45 +0200

    fixes

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.tokyonight" },
-  { import = "astrocommunity.colorscheme.oxocarbon" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
